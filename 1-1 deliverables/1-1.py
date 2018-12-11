80aae17219b9f20aa2235d125c4c89ea7166f220
jennafolsom <21folsje@wtahs.com>
2018-12-11 09:29:07 -0500

    organized things

---
+++ 1-1 deliverables/1-1.py
@@ -0,0 +1,12 @@
+# File: 1-1.py
+# Created on 11/19/18 by Jenna Folsom
+# Tests different outcomes
+
+print("Hello, world!")
+print("Hello", "world!")
+print(3)
+print(3.0)
+print(2 + 3)
+print(2.0 + 3.0)
+print("2" + "3")
+print("2 + 3 =", 2 + 3)
