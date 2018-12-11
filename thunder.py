b2691e9943374f78555fd5e8463b766d52cc8f44
folsomjenna <45171767+folsomjenna@users.noreply.github.com>
2018-12-11 07:51:05 -0500

    Add files via upload

--- thunder.py
+++ thunder.py
@@ -7,4 +7,16 @@ def main():
     import math
 
     # this is an introduction
-    print("This is a program that determines the distance to a lightning strike based on the time elapsed between the flash and the sound of thunder.")
+    print("This is a program that determines the distance to a lightning strike")
+    print("based on the time elapsed between the flash and the sound of thunder.")
+    print("                                                                     ")
+
+    # this is an input
+    distance = eval(input("How many seconds were between the flash and the sound of thunder? "))
+    total = distance * 1100
+    miles = round(total / 5280 , 2)
+
+    # this is an output
+    print("The distance to the lightning strike is", miles, "miles.")
+
+main()                    
