61a9a7e44fa848c35ceb0632504c6386130010ea
jennafolsom <21folsje@wtahs.com>
2018-12-11 10:27:26 -0500

    organized

---
+++ 1-3 deliverables/ladder.py
@@ -0,0 +1,20 @@
+# ladder.py
+# Created on 12/11/18 by Jenna Folsom
+# A program that determines the length of a ladder required given a height when leaned against a house
+
+def main():
+
+    import math
+
+    # This is an introduction
+    print("This is a program that determines the length of a ladder required given a height when leaned against a house.")
+
+    # This is an input
+    height = eval(input("What is the height of the ladder? "))
+    angle = eval(input("what is the angle of the ladder? "))
+    length = round(abs((height) / math.sin(angle)) , 2)
+
+    # This is an output
+    print("The lenght of the ladder is", length, ".")
+
+main()    
