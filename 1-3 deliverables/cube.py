31d48b91376d173b205dcd64d60408ae032d7140
jennafolsom <21folsje@wtahs.com>
2018-12-11 11:01:27 -0500

    organized

--- 1-3 deliverables/cube.py
+++ 1-3 deliverables/cube.py
@@ -1,13 +1,13 @@
-# natural.py
+# cube.py
 # Created on 12/11/18 by Jenna Folsom
-# A program to find the sum of the first n natural numbers.
+# A program to find the sum of the cubes of the first n natural numbers.
 
 def main():
 
     import math
 
     # This is an introduction
-    print("This is a program to find the sum of the first n natural number.")
+    print("This is a program to find the sum of the cubes of the first n natural number.")
 
     # This is an input
     n = eval(input("What would you like to find the sum of? "))
@@ -16,9 +16,9 @@ def main():
 
     # This is an output
     for i in range(n):
-        t = t + a
+        t = t + a **3
         a = a + 1
 
     print("The sum of the first natural numbers is", t, ".")
 
-main()    
+main()
